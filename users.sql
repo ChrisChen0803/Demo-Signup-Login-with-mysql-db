f94451b83d4612976cb2076baa2d2b008fb09244
ChrisChen0803 <78402800+ChrisChen0803@users.noreply.github.com>
2023-07-12 17:04:33 -0700

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `fullName` varchar(255) DEFAULT NULL,
+  `emailAddress` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+   `country` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+USE `usersinfo`;
